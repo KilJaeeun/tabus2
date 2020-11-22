06c549fef8328ef8a662aef3a955938249e7110f
Kil <rha3122@naver.com>
2020-11-22 13:38:02 +0900

    안쓰는 html 지우기

---
+++ static/common.js
@@ -0,0 +1,23 @@
+var DELAY = 700, clicks = 0, timer = null;
+
+$(function () {
+    $("button")
+        .on("dblclick", function (e) {
+            e.preventDefault();  //cancel system double-click event
+        });
+});
+
+function speech(obj) {
+    //one click
+    var is = true;
+
+    var utterThis = new SpeechSynthesisUtterance(obj.innerText);
+    var synth = window.speechSynthesis;
+    synth.speak(utterThis);
+    console.log("work")
+    return is;
+}
+
+function goto(place) {
+    return location.href = place;
+}
